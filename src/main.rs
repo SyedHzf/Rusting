db5f003a2d3df1c904870262c165c012b407f76f
Syed Huzaifa <rustingwork@gmail.com>
2021-09-25 23:53:50 +0500

    Concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,19 @@
 #![warn(non_snake_case)]
-//mod types;
-//mod vars;
-//mod print;
-//mod strings;
-//mod tuples;
-//mod array;
-// mod vectors;
-// mod functions;
-// mod factorial; 
-// mod fibbo;
-// mod struc;
-// mod String_vs_str;
-// mod Associated_functions_and_Methods;
-// mod Box_pointer;
-// mod Deref;
-// mod pointers;
-// mod MyBox;
-// mod Drop_pointer;
-mod refcell;
+//mod types;//mod vars;
+//mod print;//mod strings;
+//mod tuples;//mod array;
+// mod vectors;// mod functions;
+// mod factorial; // mod fibbo;// mod struc;
+// mod String_vs_str;// mod Associated_functions_and_Methods;
+// mod Box_pointer;// mod Deref;
+// mod pointers;// mod MyBox;
+// mod Drop_pointer;// mod refcell;
 
+// mod creating_thread;//mod messages_passing;
+mod sharing_state;
 fn main() {
  
   // MyBox::run();
-  refcell::run();
+  sharing_state::run();
 
   }
